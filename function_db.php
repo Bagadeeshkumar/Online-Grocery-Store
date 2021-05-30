16d5f9f79ed02f839993aec2712bb88e3a5b8013
Bagadeeshkumar P <62175685+Bagadeeshkumar@users.noreply.github.com>
2021-05-30 21:13:57 +0530

    Update function_db.php

--- function_db.php
+++ function_db.php
@@ -16,8 +16,8 @@ function mailTo($to, $user, $subject, $body, $success, $error)
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'projectdemo1310@gmail.com';
-        $mail->Password = 'project@demo';
+        $mail->Username = '';//your mail id
+        $mail->Password = '';//your password
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
